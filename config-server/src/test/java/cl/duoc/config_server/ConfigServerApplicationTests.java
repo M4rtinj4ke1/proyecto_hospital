92350a800eb32b86a31d6070fab290159df777b1
Martin Jaque <M4rtinj4ke1>
2026-06-17 15:17:36 -0400

    Docker funcionando y todo con YML

--- config-server/src/test/java/cl/duoc/config_server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/cl/duoc/config_server/ConfigServerApplicationTests.java
@@ -2,7 +2,7 @@ package cl.duoc.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@EnableConfigServer
+
 @SpringBootTest
 class ConfigServerApplicationTests {
 
--- pagos-service/src/main/java/cl/duoc/pagos_service/exception/ErrorResponse.java
+++ pagos-service/src/main/java/cl/duoc/pagos_service/exception/ErrorResponse.java
@@ -2,10 +2,12 @@ package cl.duoc.pagos_service.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private int status;
     private String mensaje;
