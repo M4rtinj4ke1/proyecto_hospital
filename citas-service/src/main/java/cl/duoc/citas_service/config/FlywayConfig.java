d7b1410739c05387d7cfbe1b69ff1040d064ae11
Martin Jaque <M4rtinj4ke1>
2026-06-18 00:36:20 -0400

    Swagger funcionando, Responder dudas sobre Citas por el codigo

---
+++ citas-service/src/main/java/cl/duoc/citas_service/config/FlywayConfig.java
@@ -0,0 +1,22 @@
+package cl.duoc.citas_service.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public Flyway flywayInitializer(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+}
